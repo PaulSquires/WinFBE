0017b2f0b26c859712b59ec5eaba7d4dcffe6a63
Paul Squires <planetsquires@gmail.com>
2025-05-21 20:58:40 -0230

    20250521

--- _compile32bit.bat
+++
@@ -1,6 +0,0 @@
-set FBLIBDIR=win32
-set FBCPATH=%~d0\PSS121\FB\WinFBE_Suite\Toolchains\FreeBASIC-1.10.0-winlibs-gcc-9.3.0
-
-cd src
-%FBCPATH%\fbc32 WinFBE.bas -x ..\WinFBE32.exe WinFBE.rc -s gui
-cd ..
--- _compile64bit.bat
+++
@@ -1,6 +0,0 @@
-set FBLIBDIR=win64
-set FBCPATH=%~d0\PSS121\FB\WinFBE_Suite\Toolchains\FreeBASIC-1.10.0-winlibs-gcc-9.3.0
-
-cd src
-%FBCPATH%\fbc64 WinFBE.bas -x ..\WinFBE64.exe WinFBE.rc -s gui -gen gcc -O 2
-cd ..
